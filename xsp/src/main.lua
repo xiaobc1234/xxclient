c7a2ca4a9d53469ff5c9bd270eb68a0999092215
肖兵成 <bingcheng.xiao@chinaredstar.com>
2017-07-19 12:34:18 +0800

    传奇无双定制脚本

--- xsp/src/main.lua
+++ xsp/src/main.lua
@@ -8,11 +8,11 @@ local strUtils = bb.getStrUtils()
 
 showTip("当前分辨率：".._fsw.."*".._fsh)
 
-deviceId= string.format('%s', getDeviceUUID())
-
 
 -- 网络版
 local function serverMain()
+
+	deviceId= string.format('%s', getDeviceUUID())
 
 	dialog("欢迎使用冰城脚本！您的设备号: "..deviceId)
 
@@ -51,20 +51,7 @@ end
 -- 脱机版
 local function clientOnlyMain()
 	
-	--首先移动浮层
-	移动浮层()
-	
-	local _回收装备全 = config['回收装备全']
-	local _领取回收奖励 = config['领取回收奖励']
-	
-	if _回收装备全=='0' then
-		回收装备全()
-	end
-
---	print("main="..config['main'])
-	if config['main']=='0' then
-		主线任务()
-	end
+	登录()
 	
 	local 每日必做 = config['每日必做'];
 	local must={}
@@ -77,23 +64,19 @@ local function clientOnlyMain()
 	print(must)
 	for i,v in ipairs(must) do
 		if "0"==v then
-			--除魔12
-			除魔12()
+			除魔5次()
 		elseif "1"==v then
-			--个人BOSS
-			每日必做_个人BOSS()
-		elseif "2"==v then
-			--通天塔
 			通天塔2次()
+		elseif "2"==v then
+--		TODO
+--			膜拜城主()
 		elseif "3"==v then
-			--膜拜城主
-			膜拜城主()
+			每日必做副本()
 		end
   end
-	if must then
-		-- 领取每日必做奖励
-		领取每日必做奖励()
-	end
+	
+	-- 领取每日必做奖励
+	每日必做领奖()
 	
 	--下面是打经验的脚本
 	local 个人BOSS = config['个人BOSS'];
@@ -106,34 +89,23 @@ local function clientOnlyMain()
 	print(boss)
 	for i,v in ipairs(boss) do
 		if "0"==v then
-			--牛魔
-			个人BOSS牛魔()
+			个人BOSS沃玛()
 		elseif "1"==v then
-			--魔龙
-			个人BOSS魔龙()
+			个人BOSS祖玛()
 		elseif "2"==v then
-			--火龙
-			个人BOSS火龙()
+			个人BOSS赤月()
+		elseif "3"==v then
+			个人BOSS魔龙()
 		end
   end
 	
-	--开始一次回收，结束一次回收
-	if _回收装备全=='0' then
-		回收装备全()
+-- 领取每日必做奖励
+		每日必做领奖()
+		
+	if 切换角色() then
+	--	如果不是第四个角色执行完，就切换角色重新执行
+		clientOnlyMain()
 	end
-	
-	if _领取回收奖励=='0' then
-		领取回收奖励()
-	end
-	
-	if config['未知暗殿']=="0" then
-		--先召唤战神
-		未知暗殿挂机(1)-- 刷排序中的第一个boss
-	elseif config['未知暗殿']=="1" then
-		未知暗殿挂机(2)
-	end
-	
-
 	
 end
 
--- xsp/src/service/MainService.lua
+++ xsp/src/service/MainService.lua
@@ -3,7 +3,60 @@ local dao = require "data/DataCqws"
 
 
 --------------------功能函数------------------------------
-function 主线任务()
-	登录()
-	主线()
+
+function 登录()
+	dao:登录()
 end
+
+function 除魔5次()
+	dao:降妖除魔5()
+end
+
+function 通天塔2次()
+	dao:通天塔2次()
+end
+
+-- 必须在通天塔做完之后才可以做
+function 快捷喝酒5次()
+	dao:每日必做_喝酒()
+end
+
+function 个人BOSS沃玛()
+	for tmpi=1,3 do
+		dao:个人BOSS_沃玛()        
+		sleep('个人BOSS结束',3000)
+  end
+end
+
+function 个人BOSS祖玛()
+	for tmpi=1,3 do
+		dao:个人BOSS_祖玛()       
+		sleep('个人BOSS结束',3000)
+  end
+end
+function 个人BOSS赤月()
+	for tmpi=1,2 do
+		dao:个人BOSS_赤月()       
+		sleep('个人BOSS结束',3000)
+  end
+end
+
+function 个人BOSS魔龙()
+	dao:个人BOSS_魔龙()
+end
+function 每日必做领奖()
+	dao:每日必做_领取奖励()
+end
+
+function 每日必做副本()
+	dao:每日必做_副本(1)
+	dao:每日必做_副本(2)
+	dao:每日必做_副本(3)
+	dao:每日必做_副本(4)
+end
+
+function 切换角色()
+	return dao:切换角色()
+end
+
+
